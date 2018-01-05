dffdbfb7b6c1a53750aa32d75f2b29ed5e6d869d
Devil7 <dineshthangavel47@gmail.com>
2018-01-05 17:35:48 +0000

    haxx:Don't warn user for inconsistent build fingerprint

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1846,7 +1846,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                     d.show();
                 }
             } break;
-            case SHOW_FINGERPRINT_ERROR_UI_MSG: {
+            /*case SHOW_FINGERPRINT_ERROR_UI_MSG: {
                 if (mShowDialogs) {
                     AlertDialog d = new BaseErrorDialog(mUiContext);
                     d.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ERROR);
@@ -1857,7 +1857,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                             obtainMessage(DISMISS_DIALOG_UI_MSG, d));
                     d.show();
                 }
-            } break;
+            } break;*/
             case SHOW_COMPAT_MODE_DIALOG_UI_MSG: {
                 synchronized (ActivityManagerService.this) {
                     ActivityRecord ar = (ActivityRecord) msg.obj;
@@ -14286,10 +14286,10 @@ public class ActivityManagerService extends IActivityManager.Stub
             } catch (RemoteException e) {
             }
 
-            if (!Build.isBuildConsistent()) {
+            /*if (!Build.isBuildConsistent()) {
                 Slog.e(TAG, "Build fingerprint is not consistent, warning user");
                 mUiHandler.obtainMessage(SHOW_FINGERPRINT_ERROR_UI_MSG).sendToTarget();
-            }
+            }*/
 
             long ident = Binder.clearCallingIdentity();
             try {
